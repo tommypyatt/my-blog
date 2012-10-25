9ae49b01d92d80788cbaa7bdbde8b1b266326baf
tommypyatt <tommy.pyatt@googlemail.com>
2012-10-25 19:26:25 +0100

    404 error page text

--- wp-content/themes/tpyatt.com/404.php
+++ wp-content/themes/tpyatt.com/404.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+	<div id="content-main-wrap">
+		<h2>404: Page Not Found</h2>
+		<p>That page doesn't exist on this site. You can go to my <a href="<?php echo get_option('home'); ?>/">homepage</a> or use the search bar at the top of the page to try to find the information you are looking for.</p>
+	</div>
 
 <?php get_sidebar(); ?>
 
