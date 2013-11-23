872c81a4c5b179f16e56eceaccf8bf1cd936fa29
Tommy Pyatt <tommy.pyatt@googlemail.com>
2013-11-23 13:48:21 +0000

    Add links to search results page

--- wp-content/themes/tpyatt.com/search.php
+++ wp-content/themes/tpyatt.com/search.php
@@ -4,17 +4,13 @@
 
 	<?php if (have_posts()) : ?>
 
-		<h2>Search Results</h2>
-
-		<div class="meta-top">
-			<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
-		</div>
+		<h2 class="search-header"><?php printf( __( 'Search Results for &ldquo;%s&rdquo;', 'twentytwelve' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
 
 		<?php while (have_posts()) : the_post(); ?>
 
 			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-				<h2><?php the_title(); ?></h2>
+				<h2 class="post-header"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
 				<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 
