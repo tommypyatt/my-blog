68e4b51b26ccbfc4f271c10b40c73b1c9cb34202
Tommy Pyatt <tommy.pyatt@googlemail.com>
2013-11-23 13:32:52 +0000

    Added styling for search box

--- wp-content/themes/tpyatt.com/searchform.php
+++ wp-content/themes/tpyatt.com/searchform.php
@@ -1,5 +1,6 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-    <div>
-        <input type="search" id="s" name="s" value="" placeholder="Search..."/>
+    <label for="s" class="search-label"><?php _e('Search'); ?></label>
+    <div class="search-input-wrap">
+        <input type="search" class="search-input" id="s" name="s" value="" placeholder="Search..."/>
     </div>
 </form>
